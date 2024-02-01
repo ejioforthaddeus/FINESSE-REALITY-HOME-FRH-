fe0ccbeb84fb097e6ebee8d3697dffbe158906c4
ejioforthadeus <ejioforthaddeus037@gmail.com>
2024-02-01 12:35:20 +0100

    first commit

---
+++ projects.php
@@ -0,0 +1,139 @@
+<?php
+include 'header.php'
+?>
+
+
+<div class="page-title-area title-bg-three">
+<div class="title-shape">
+<img src="assets/img/title/title-bg-shape.png" alt="Shape">
+</div>
+<div class="d-table">
+<div class="d-table-cell">
+<div class="container">
+<div class="title-content">
+<h2>Projects</h2>
+<ul>
+<li>
+<a href="home">Home</a>
+</li>
+<li>
+<i class="bx bx-chevron-right"></i>
+</li>
+<li>
+<span>Projects</span>
+</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="projects-area ptb-100">
+<div class="container">
+<div class="section-title">
+<div class="row align-items-center">
+<div class="col-lg-6">
+<span class="sub-title">Projects</span>
+<h2>Some Of Our <span>Projects</span> Where We Achive Great Success</h2>
+</div>
+<div class="col-lg-6">
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis libero facilis consequatur deleniti, ipsa provident? Culpa tenetur incidunt reprehenderit qui a voluptas perferendis officiis</p>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-sm-6 col-lg-4">
+<div class="projects-item card-overlay">
+<img src="assets/img/projects/projects1.jpg" alt="Projects">
+<div class="inner">
+<h3>
+<a href="project-details.html">Financial Plan</a>
+</h3>
+<span>Enterprise</span>
+</div>
+</div>
+</div>
+<div class="col-sm-6 col-lg-4">
+<div class="projects-item card-overlay">
+<img src="assets/img/projects/projects2.jpg" alt="Projects">
+<div class="inner">
+<h3>
+<a href="project-details.html">Business Plan</a>
+</h3>
+<span>Retail Shop</span>
+</div>
+</div>
+</div>
+<div class="col-sm-6 col-lg-4">
+<div class="projects-item card-overlay">
+<img src="assets/img/projects/projects3.jpg" alt="Projects">
+<div class="inner">
+<h3>
+<a href="project-details.html">Investment Plan</a>
+</h3>
+<span>Startup</span>
+</div>
+</div>
+</div>
+<div class="col-sm-6 col-lg-4">
+<div class="projects-item card-overlay">
+<img src="assets/img/projects/projects4.jpg" alt="Projects">
+<div class="inner">
+<h3>
+<a href="project-details.html">IT Startup</a>
+</h3>
+<span>Startup</span>
+</div>
+</div>
+</div>
+<div class="col-sm-6 col-lg-4">
+<div class="projects-item card-overlay">
+<img src="assets/img/projects/projects5.jpg" alt="Projects">
+<div class="inner">
+<h3>
+<a href="project-details.html">Support Maintain</a>
+</h3>
+<span>Envato</span>
+</div>
+</div>
+</div>
+<div class="col-sm-6 col-lg-4">
+<div class="projects-item card-overlay">
+<img src="assets/img/projects/projects6.jpg" alt="Projects">
+<div class="inner">
+<h3>
+<a href="project-details.html">Business Growth</a>
+</h3>
+<span>Business</span>
+</div>
+</div>
+</div>
+</div>
+<div class="pagination-area">
+<ul>
+<li>
+<a href="#">Prev</a>
+</li>
+<li>
+<a class="active" href="#">1</a>
+</li>
+<li>
+<a href="#">2</a>
+</li>
+<li>
+<a href="#">3</a>
+</li>
+<li>
+<a href="#">Next</a>
+</li>
+</ul>
+</div>
+</div>
+</div>
+
+
+<?php
+include 'footer.php'
+?>
